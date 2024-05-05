eee0edccd7d2a154a60798129c759c20f9463f4a
Manika <manika.aditya@gmail.com>
2024-05-06 02:10:32 +0530

    dynamic component styling

--- src/Product.jsx
+++ src/Product.jsx
@@ -4,8 +4,11 @@ function Product ({title, price, features}) {
     // const list = features.map((feature) => <li>{feature}</li>);
 
     let isDiscount = price > 30000 ? "5% Dsicount" : "";
+
+    let styles = {backgroundColor : isDiscount ? "pink" : "" };
+
         return (
-            <div className="Product">
+            <div className="Product" style={styles}>
                 <h3>{title}</h3>
                 <h5>Price : {price}</h5>
                 <p>{isDiscount}</p>

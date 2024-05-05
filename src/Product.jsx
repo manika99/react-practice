5264cfc359b408497c67f087bbb34921572b01ac
Manika <manika.aditya@gmail.com>
2024-05-06 02:01:05 +0530

    conditionals

--- src/Product.jsx
+++ src/Product.jsx
@@ -2,24 +2,15 @@ import './Product.css'
 
 function Product ({title, price, features}) {
     // const list = features.map((feature) => <li>{feature}</li>);
-    if(price>30000){
+
+    let isDiscount = price > 30000 ? "5% Dsicount" : "";
         return (
             <div className="Product">
                 <h3>{title}</h3>
                 <h5>Price : {price}</h5>
-                <p>Discount of 5%</p>
+                <p>{isDiscount}</p>
             </div>
         )
-    }
-    else{
-        return (
-            <div className="Product">
-                <h3>{title}</h3>
-                <h5>Price : {price}</h5>
-            </div>
-        )
-    }
-    
 }
 
 export default Product;
